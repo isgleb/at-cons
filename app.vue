0e8382bf91ffe6973d8b7c79823979ca2a85ed35
isgleb <71262423+isgleb@users.noreply.github.com>
2023-04-29 20:55:59 +0300

    component

--- app.vue
+++ app.vue
@@ -1,57 +1,8 @@
 <template>
   <div>
-<!--    <InputMask id="basic" v-model="value" mask="https://******.****" placeholder="https://" />-->
-<!--    <InputMask id="basic" v-model="value" mask="******.***" @blur="getTitle" placeholder="https://" />-->
-
-
-    <div class="p-inputgroup flex-1" style="width: 300px">
-      <InputText
-          :placeholder="placeHolder"
-          v-model="value"
-          @focus="focusIn"
-          @focusout="focusOut"/>
-      <Button
-          v-if="showEdit"
-          icon="pi pi-pencil"
-          severity="danger"
-      />
-    </div>
-
+    <link-input/>
   </div>
 </template>
 <script setup>
-import InputMask from 'primevue/inputmask';
-import Button from 'primevue/button'
-import InputText from 'primevue/inputtext'
-import 'primeicons/primeicons.css';
-
-const value = ref("")
-const url = ref("")
-const showEdit = ref(false)
-const placeHolder = "https://"
-
-function focusIn() {
-  showEdit.value = false
-  value.value = url.value
-}
-
-async function focusOut() {
-  url.value = value.value
-  showEdit.value = true
-
-  value.value = await getTitle(placeHolder + value.value)
-}
-
-
-async function getTitle(targetUrl) {
-  const options = {
-    params: {
-      url: targetUrl
-    }
-  }
-  const { title } = await $fetch('/api/page-title', options)
-  return title
-}
-
-
+import LinkInput from 'public/components/LinkInput'
 </script>
