cd4b7e5e5ce7797c157dfd93f0a934079499718d
isgleb <71262423+isgleb@users.noreply.github.com>
2023-04-30 11:43:43 +0300

    clearable multiselect

--- app.vue
+++ app.vue
@@ -2,9 +2,18 @@
   <div>
     <link-input/>
     <TimePicker/>
+    <ClearableMultiselect :options="cities"/>
   </div>
 </template>
 <script setup lang="ts">
-import LinkInput from './public/components/LinkInput.vue'
-import TimePicker from "./public/components/TimePicker.vue";
+import LinkInput from 'public/components/LinkInput.vue'
+import TimePicker from "public/components/TimePicker.vue";
+import ClearableMultiselect from "public/components/ClearableMultiselect.vue";
+
+const cities = [
+  {name: "Москва", value: false},
+  {name:"Рим", value: false},
+  {name: "Париж", value: false},
+  {name: "Берлин", value: false}
+]
 </script>
---
+++ public/components/ClearableMultiselect.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="p-inputgroup flex-1">
+    <MultiSelect
+        :options="options"
+        :showToggleAll=false
+        optionLabel="name"
+        v-model="selectedOptions"
+        class="w-full md:w-20rem"/>
+    <Button
+        v-if="!isSelectedEmpty"
+        @click="clear"
+        icon="pi pi-times"
+        severity="danger"
+    />
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import MultiSelect from 'primevue/multiselect';
+import {PropType} from "@vue/runtime-core";
+import {computed, ref} from "#imports";
+
+const selectedOptions = ref<Array<string>>()
+
+const isSelectedEmpty = computed(()=>{
+  return !(selectedOptions.value && selectedOptions.value.length > 0)
+})
+
+const props = defineProps({
+  options: {type: Array as PropType<string[]>}
+})
+
+function clear() {
+  selectedOptions.value = []
+}
+
+</script>
+
+<style scoped>
+
+</style>
