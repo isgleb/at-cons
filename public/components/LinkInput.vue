b4e50e0dcd902d68466b7cfe3a51c4d2a4b32f5c
isgleb <71262423+isgleb@users.noreply.github.com>
2023-05-01 13:30:16 +0300

    link input refactoring

--- public/components/LinkInput.vue
+++ public/components/LinkInput.vue
@@ -1,21 +1,20 @@
 <template>
   <div>
-    <div class="p-inputgroup flex-1" style="width: 300px">
-      <button class="link-button" v-if="isLink" >
-        <a :href="placeHolder + url">{{value}}</a>
+    <div class="p-inputgroup">
+      <button class="link-button" v-if="titleMode" >
+        <a :href="placeHolder + link">{{title}}</a>
       </button>
       <InputText
+          v-else
           :placeholder="placeHolder"
-          :class="{'p-invalid': !isValid}"
-          v-model="value"
+          :class="{'p-invalid': !isLinkValid}"
+          v-model="link"
           @focus="focusIn"
-          v-else="isLink"
           @focusout="focusOut"/>
       <Button
           v-if="showEdit"
           @click="setEditing"
           :icon="icon"
-          severity="danger"
       />
     </div>
   </div>
@@ -24,51 +23,48 @@
 import Button from 'primevue/button'
 import InputText from 'primevue/inputtext'
 import 'primeicons/primeicons.css';
-import {computed} from "#imports";
+import {computed, ref} from "#imports";
 
 const placeHolder = "https://"
-const value = ref("")
-const url = ref("")
+
+const title = ref("")
+const link = ref("")
+
 const showEdit = ref(false)
-const isValid = ref(true)
+const isLinkValid = ref(true)
 const isLoading = ref(false)
-const isLink = ref(false)
+const titleMode = ref(false)
 
 const icon = computed(()=>{
   return isLoading.value ? "pi pi-spin pi-spinner" : "pi pi-pencil"
 })
 
 function setEditing(){
-  value.value = url.value
-  isLink.value = false
+  titleMode.value = false
 }
 
 function focusIn() {
   showEdit.value = false
-  isValid.value = true
-  // value.value = url.value
+  isLinkValid.value = true
 }
 
 async function focusOut() {
-  url.value = value.value
   showEdit.value = true
-
-  const params = {
-    url: placeHolder + value.value
+  const options = {
+    params: {
+      url: placeHolder + link.value
+    }
   }
-
   isLoading.value = true
-  isValid.value = true
+  isLinkValid.value = true
   try {
-    const title = await $fetch<string>('/api/page-title', { params: params } )
-    value.value = title
-    isLink.value = true
+    title.value = await $fetch<string>('/api/page-title', options )
+    titleMode.value = true
   } catch(error) {
-    isValid.value = false
+    isLinkValid.value = false
   } finally {
     isLoading.value = false
   }
-
 }
 </script>
 <style>
