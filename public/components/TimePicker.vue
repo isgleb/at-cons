31de95a98b65c593a31e410f4d5be1e9c9793f80
isgleb <71262423+isgleb@users.noreply.github.com>
2023-05-02 18:05:30 +0300

    timepicker

--- public/components/TimePicker.vue
+++ public/components/TimePicker.vue
@@ -1,5 +1,5 @@
 <template>
-  <calendar v-model="time" timeOnly>
+  <calendar :modelValue="date" @update:modelValue="update" timeOnly>
     <template #footer>
       <hr>
       <div class="controls">
@@ -12,16 +12,37 @@
 
 <script setup lang="ts">
 import Calendar from 'primevue/calendar';
-import {ref} from "#imports";
+import { computed } from "#imports";
 
-const time = ref()
+const props = defineProps({
+  time: { type: String, required: false }
+})
+
+const emits = defineEmits(["update:time"])
+
+const date = computed(()=>{
+
+  if (props.time && /\d{1,2}:\d{1,2}/.test(props.time)) {
+    const d = new Date()
+    d.setHours(parseInt(props.time.split(":")[0]))
+    d.setMinutes(parseInt(props.time.split(":")[1]))
+    return d;
+  } else {
+    return null
+  }
+})
+
+function update(date: Date) {
+  const stringValue = `${date.getHours()}:${date.getMinutes()}`
+  emits("update:time", stringValue)
+}
 
 function clearTime() {
-  time.value = null
+  emits("update:time", null)
 }
 
 function setTimeNow(){
-  time.value = new Date()
+  update(new Date)
 }
 
 </script>
